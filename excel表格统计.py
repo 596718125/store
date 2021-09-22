33c4dbdbd669dee45176e3560212cd618a885ac0
wyt <596718125@qq.com>
2021-09-22 10:45:44 +0800

    day8-任务1

--- excel表格统计.py
+++ excel表格统计.py
@@ -195,29 +195,11 @@ def year_min():
 #运行year_min()
 
 
-# 任务2：把excel表的所有数据存储到数据库。
-def my(sql,param):
-    con = pymysql.connect(host="localhost",user="root",password="",database="company")
-    cursor = con.cursor()
-    cursor.execute(sql,param)
-    con.commit()
-    cursor.close()
-    con.close()
-    return
-
-#执行添加数据库
-# for i in moon:
-#     sheet = wb.sheet_by_name(i)
-#     rows = sheet.nrows
-#     for j in range(1, rows):
-#         data = sheet.row_values(j)
-#         print(data)
-#         sql = "insert into clothes values(%s,%s,%s,%s,%s,%s)"
-#         param = [i,data[0],data[1],data[2],data[3],data[4]]
-#         my(sql, param)
 
 
-# 任务3：将三国集团数据，导出到excel里。
+
+
+
 
 
 
